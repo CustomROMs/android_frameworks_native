0905e8c6eae7e43ade0f83eb7cebfa363f01308d
Valerie Hau <vhau@google.com>
2018-10-09 16:05:09 -0700

    Support DISPLAY_P3 dataspace to BT2020 in SF
    Adding support to fallback to ColorMode BT2020 with
    SRGB when P3 layers are present when external display supports BT2020 with SRGB
    transfer function.  (Before fallback was to BT709_sRGB)
    
    Bug: 115335239
    Test: ./libsurfaceflinger_unittest --gtest_filter=GetBestColorModeTest.*
    Change-Id: Iec7154d411cb3dcc4fd1158199bc45207799bb27

--- libs/ui/include/ui/GraphicTypes.h
+++ libs/ui/include/ui/GraphicTypes.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <android/hardware/graphics/common/1.1/types.h>
+#include <android/hardware/graphics/common/1.2/types.h>
 #include <system/graphics.h>
 
 #define ANDROID_PHYSICAL_DISPLAY_ID_FORMAT PRIu64
@@ -30,10 +31,10 @@ using PhysicalDisplayId = uint64_t;
 namespace ui {
 
 using android::hardware::graphics::common::V1_0::Hdr;
-using android::hardware::graphics::common::V1_1::ColorMode;
-using android::hardware::graphics::common::V1_1::Dataspace;
 using android::hardware::graphics::common::V1_1::PixelFormat;
 using android::hardware::graphics::common::V1_1::RenderIntent;
+using android::hardware::graphics::common::V1_2::ColorMode;
+using android::hardware::graphics::common::V1_2::Dataspace;
 
 }  // namespace ui
 }  // namespace android
