f1139a25d12fa4d2a53250922705acd7b0a89527
Shilin Victor <chrono.monochrome@gmail.com>
2020-06-16 10:13:36 +0300

    libui: add stub isSupported() method to GraphicBuffer
    Change-Id: Ib62dd73462fd6ef7ad6b8d1b622edb7998523fdb

--- libs/ui/GraphicBufferMapper.cpp
+++ libs/ui/GraphicBufferMapper.cpp
@@ -211,5 +211,10 @@ status_t GraphicBufferMapper::unlockAsync(buffer_handle_t handle, int *fenceFd)
     return err;
 }
 
+status_t GraphicBufferMapper::isSupported(uint32_t /*width*/, uint32_t /*height*/,
+                                          android::PixelFormat /*format*/, uint32_t /*layerCount*/,
+                                          uint64_t /*usage*/, bool* /*outSupported*/) const {
+    return INVALID_OPERATION;
+}
 // ---------------------------------------------------------------------------
 }; // namespace android
